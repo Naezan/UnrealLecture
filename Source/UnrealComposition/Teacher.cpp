7206762856f86b487a14e95678a37b548189274e
Naezan <64336496+Naezan@users.noreply.github.com>
2023-03-26 10:26:17 +0900

    UnrealComposition

---
+++ Source/UnrealComposition/Teacher.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Teacher.h"
+#include "Card.h"
+
+UTeacher::UTeacher()
+{
+	Name = TEXT("내잔선생");
+	Card->SetCardType(ECardType::Teacher);
+}
+
+void UTeacher::DoLesson()
+{
+	ILessonInterface::DoLesson();
+
+	UE_LOG(LogTemp, Log, TEXT("%s님은 수업을 합니다."), *Name);
+}
